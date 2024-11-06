65f590b098de8f971ebed0e995a6cf25138cbd71
Cesare <75073012+cheng-wei-huang0612@users.noreply.github.com>
2024-11-06 19:27:00 +0800

    Modified assembly ntt

---
+++ NTT_assembly/data.h
@@ -0,0 +1,12 @@
+// data.h
+#ifndef DATA_H
+#define DATA_H
+
+#include <stdint.h>  // Include this to define int32_t, uint16_t, and uint8_t
+
+extern int32_t g[1024];
+extern int32_t f[1024];
+extern uint16_t n;
+extern uint8_t logn;
+
+#endif // DATA_H
